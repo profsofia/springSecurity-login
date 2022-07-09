58a4623c42f796d8938c1a241bf980b175145eec
sofia <sofiainesschenone@gmail.com>
2022-07-09 18:04:42 -0300

    primer commit

---
+++ src/test/java/com/sofiafullstack/LOGIN/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofiafullstack.LOGIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
